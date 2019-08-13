6db6d19647aeaf37ad5b1101a7713d7a98bf57a7
nolan <nolanpokpongkiat@berkeley.edu>
2019-08-12 21:50:43 -0700

    renamed files

---
+++ 1-install-packages.R
@@ -0,0 +1,10 @@
+#######################
+#  Package Install
+# ---------------------
+# Install packages
+install.packages("here",
+                 "BiocManager",
+                 "Seurat",
+                 "cowplot",
+                 "dplyr",
+                 "reticulate")
